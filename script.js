ada2431db5554c25d4bd3521beabdbc30ed59966
Preent Loo <psilocybit5211@gmail.com>
2022-06-11 18:11:32 -0400

    Create and Style Foods Gallery.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-/*Select ALL the icons*/
+/*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
+          S E C T I O N   0 1
+        C H A N G E   I C O N S
+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*/
+
+/*
+<div class="section-01-icons">
+    <i> All  "i"  elements </i>
+</div>*/
 const icons = document.querySelectorAll(".section-01-icons i");
 
-/*Fixing the next of the last element*/
-/*Once the let "i" is gretaer than the length
+/*Fixing the next of the last element.
+Once the let "i" is gretaer than the length
 of the icons, we should set the counter (i) back to one*/
 let i = 1;
 
-/*Manipulate the appears and desappears.
+/*Manipulate the icons appears 
+and desappears, with class "change".
 In two thousand five hundred miliseconds (2500)*/
 setInterval(() => {
     i++
@@ -14,9 +23,13 @@ setInterval(() => {
     /*Remove "change" class and put it in the next sibling element*/
     icon.classList.remove("change");
     if(i > icons.length){
+        // 1st of div container
         icons[0].classList.add("change");
         i = 1;
     } else {
         icon.nextElementSibling.classList.add("change");
     };
 }, 2500);
+/*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
+        E N D  S E C T I O N   0 1
+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*/
